912d3c8540703082bc32b6eb58d4200170635f79
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-09-13 18:12:47 +0200

    implementing slider for time, need to change keys to match an even interval

--- plot_int.py
+++ plot_int.py
@@ -7,16 +7,16 @@ from bokeh.layouts import column, row, widgetbox, layout
 from bokeh.models import CustomJS, ColumnDataSource, Slider, TextInput, Select
 from bokeh.plotting import Figure, output_file, show, reset_output
 
-mz_plot = ['174.0','175.0','176.0','177.0','178.0']
+mz_plot = ['174.0','175.0','176.0','177.0']
 time_key_plot = 't738.54700000000003'
-mz_colors = ['red','blue','green','purple','orange']
+mz_colors = ['red','blue','green','purple']
 file_directory = '/Users/nate/Desktop/netcdf_test/'
 filename = 'tbdms01_t47d_wt.CDF'
 output_data_file = file_directory + 'processed_data.p'
 output_plot_file = file_directory + 'plot1.html'
 
 output_file(output_plot_file)
-plot=Figure(title='ion counts vs. time', x_axis_label='time (s)',y_axis_label='ion counts',plot_width=800,plot_height=350)
+plot=Figure(title='ion counts vs. time', x_axis_label='time (s)',y_axis_label='ion counts',plot_width=950,plot_height=350)
 
 file_object = open(output_data_file,'rb')
 file_data = pickle.load(file_object)
@@ -53,12 +53,11 @@ source_dict['y0'] = source_dict[mz_plot[0]] #the ics for the current mz
 source_dict['y1'] = source_dict[mz_plot[1]]
 source_dict['y2'] = source_dict[mz_plot[2]]
 source_dict['y3'] = source_dict[mz_plot[3]]
-source_dict['y4'] = source_dict[mz_plot[4]]
 source = ColumnDataSource(data=source_dict) #for bokeh widgets it is stored in a ColmnDataSource object
 
-y = ['y0','y1','y2','y3','y4']
+y = ['y0','y1','y2','y3']
 
-for j in [0,1,2,3,4]:
+for j in [0,1,2,3]:
     plot.line('x',y[j],source=source,color=mz_colors[j]) #update the plot object for the current mz
     #mz_text[j] = TextInput(title=mz_colors[j], value=str(mz_plot[j])) #the textbox widget, the value must be a string
     mz_text[j] = Select(title=mz_colors[j], value=str(mz_plot[j]), options=list(source_dict.keys()))
@@ -111,24 +110,11 @@ callback3 = CustomJS(args=dict(source=source), code="""
     source.change.emit();
 """)
 
-callback4 = CustomJS(args=dict(source=source), code="""
-    var data = source.data;
-    var f = cb_obj.value
-    x = data['x']
-    y4 = data['y4']
-    to_change_to = data[f]
-    for (i = 0; i < x.length; i++) {
-        y4[i] = to_change_to[i]
-    }
-    source.change.emit();
-""")
-
 #inform bokeh to update the plot based on textbox values
 mz_text[0].js_on_change('value', callback0)
 mz_text[1].js_on_change('value', callback1)
 mz_text[2].js_on_change('value', callback2)
 mz_text[3].js_on_change('value', callback3)
-mz_text[4].js_on_change('value', callback4)
 
 #Make the ion-count vs. mz plot for each scan acquisition time
 source_dict_timekeys = file_data[filename]['ics_smooth_timekeys']
@@ -148,10 +134,24 @@ test_time_value = list(source_dict_timekeys.keys())[0]
 source_dict_timekeys['y'] = source_dict_timekeys[test_time_value]
 
 source_timekeys = ColumnDataSource(data=source_dict_timekeys)
-plot2 = Figure(title='ion counts vs. mz', x_axis_label='m/z',y_axis_label='ion counts',plot_width=800,plot_height=350)
+plot2 = Figure(title='ion counts vs. mz', x_axis_label='m/z',y_axis_label='ion counts',plot_width=950,plot_height=350)
 plot2.vbar(x='x', bottom=0, width=0.5, top='y',color='firebrick',source=source_timekeys)
 
+callback6 = CustomJS(args=dict(source=source_timekeys), code="""
+     var data = source.data;
+     var f = cb_obj.value
+     f_s = f.toString()
+     x = data['x']
+     y = data['y']
+     to_change_to = data[f_s]
+     for (i = 0; i < x.length; i++) {
+         y[i] = to_change_to[i]
+     }
+     source.change.emit();
+ """)
+
 time_select = Select(title="Scan Acquisition Time:", value=test_time_value, options=list(source_dict_timekeys.keys()))
+time_slider = Slider(start=395.572, end=2716.746, value=395.572, step=0.343, title='Scan Acquisition Time', callback=callback6, callback_policy='mouseup')
 
 callback5 = CustomJS(args=dict(source=source_timekeys), code="""
      var data = source.data;
@@ -166,15 +166,22 @@ callback5 = CustomJS(args=dict(source=source_timekeys), code="""
  """)
 
 time_select.js_on_change('value', callback5)
+#time_slider.js_on_change('value', callback6)
 
 # Set up layouts and add to document
-text_boxes = widgetbox(mz_text[0],mz_text[1],mz_text[2],mz_text[3],mz_text[4],width=200,height=350)
+text_box0 = widgetbox(mz_text[0], width=100, height=20)
+text_box1 = widgetbox(mz_text[1], width=100, height=20)
+text_box2 = widgetbox(mz_text[2], width=100, height=20)
+text_box3 = widgetbox(mz_text[3], width=100, height=20)
 time_box = widgetbox(time_select,width=200,height=350)
+time_slider_box = widgetbox(time_slider,width=1000,height=30)
 #layout = row(text_boxes, plot, plot2)
 
 l = layout([
-  [text_boxes, plot],
-  [time_box,plot2],
+  [text_box0,text_box1,text_box2,text_box3],
+  [plot],
+  [time_slider_box],
+  [plot2],
 ], sizing_mode='fixed')
 
 show(l)
