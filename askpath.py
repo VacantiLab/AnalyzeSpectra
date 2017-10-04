94a58fa3e4f0978c52769c04fedd524bc0d70f86
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-10-04 14:46:42 +0200

    deleted test script

--- askpath.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk
-from tkinter.filedialog import askdirectory
-
-root = Tk()
-root.withdraw()
-
-directory = askdirectory() + '/'
-root.update() #required so the directory request dialog box disappears and does not freeze
