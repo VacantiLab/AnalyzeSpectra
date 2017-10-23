57e6251418621832f2c56f393833160cb4e71cba
Nathaniel Vacanti <nathaniel.vacanti@gmail.com>
2017-10-23 23:00:34 +0200

    plot_int now works without an internet connection

--- plot_int.py
+++ plot_int.py
@@ -25,7 +25,10 @@ filename = 'alkanes.CDF'
 output_data_file = file_directory + 'processed_data.p'
 output_plot_file = file_directory + 'plot1.html'
 
-output_file(output_plot_file, title=filename) #title sets the title shown on the browser tab
+output_file(output_plot_file, title=filename, mode='inline')
+#    title sets the title shown on the browser tab
+#    mode=inline allows it to produce the plot without being connected to the internet
+#        CDN loading is the default and requires an internet connection
 plot=Figure(title='ion counts vs. time', x_axis_label='time (s)',y_axis_label='ion counts',plot_width=950,plot_height=300)
 
 file_object = open(output_data_file,'rb')
