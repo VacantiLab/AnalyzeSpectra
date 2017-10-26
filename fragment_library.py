cb6ea20db164e17ef4f4f0d8afdd21f6d5b0b649
Nathaniel Vacanti <VacantiLab@users.noreply.github.com>
2017-10-26 16:56:38 +0200

    working on library format

--- fragment_library.py
+++ fragment_library.py
@@ -10,8 +10,9 @@ def fragment_library():
     import create_correction_matrix
     importlib.reload(create_correction_matrix)
 
-    file_name_read = '/Users/Nate/Desktop/netcdf_test/tbdms_lib.txt'
-    fragment_dict = dict()
+    #file_name_read = '/Users/Nate/Desktop/netcdf_test/tbdms_lib.txt'
+    file_name_read = '/Users/Nate/Dropbox/Research/Lehtio_Laboratory/Projects/metabolite_integration_tool/netcdf_test/library.txt'
+    metabolite_dict = dict()
 
     #open a .txt file with the fragment information and import it
     #.txt file has format:
@@ -24,44 +25,54 @@ def fragment_library():
     #    ...
     with open(file_name_read, 'r') as read_file:
         #read through the lines in the file one by one
-        outer_read_line = 0
+        file_line_n = 0
         for line in read_file:
-            outer_read_line = outer_read_line + 1 #iterate the line number
+            file_line_n = file_line_n + 1 #iterate the line number
             line_split = line.split(':') #split the line into a list of strings, colons mark separations
             #if the first word on the line is 'fragment', gather the fragment information
-            if line_split[0]=='fragment':
-                fragment_name = line_split[1].lstrip().rstrip() #remove white space characters from the left and right of the fragment name
-                print('    ' + fragment_name)
-                fragment_dict[fragment_name] = dict() #initialize a dictionary for the current fragment
-                fragment_dict[fragment_name]['areas'] = np.array([]) #initialize the peak areas dictionary
-                fragment_dict[fragment_name]['mid'] = np.array([]) #initialize the MID dictionary
-                fragment_dict[fragment_name]['mid_c'] = np.array([]) #initialize the corrected MID dictionary
-                #read through the same file line by line, but starting from the beginning
-                inner_read_line = 0
-                with open(file_name_read, 'r') as inner_read_file:
-                    for inner_line in inner_read_file:
-                        inner_read_line = inner_read_line + 1 #iterate the line number
-                        inner_line_split = inner_line.split(':') #split the line into a list of strings
-                        #read the fragment formula
-                        if inner_read_line == outer_read_line + 1:
-                            fragment_formula = inner_line_split[1].lstrip().rstrip()
-                            fragment_dict[fragment_name]['formula'] = fragment_formula
-                            fragment_dict[fragment_name]['CM_i'] = create_correction_matrix.create_correction_matrix(fragment_formula)
-                        #read the fragment retention time
-                        if inner_read_line == outer_read_line + 2:
-                            retention_time = inner_line_split[1].lstrip().rstrip()
-                            retention_time = np.fromstring(retention_time,dtype=float,sep=' ')
-                            fragment_dict[fragment_name]['rt'] = retention_time
-                        #read the fragment list of mz values to integrate
-                        if inner_read_line == outer_read_line + 3:
-                            mzs_to_integrate = inner_line_split[1].lstrip().rstrip()
-                            mzs_to_integrate = np.fromstring(mzs_to_integrate,dtype=float,sep=' ')
-                            fragment_dict[fragment_name]['mz'] = mzs_to_integrate
+            if line_split[0]=='metabolite':
+                metabolite_name = line_split[1].lstrip().rstrip() #remove white space characters from the left and right of the fragment name
+                print('    ' + metabolite_name)
+                metabolite_dict[metabolite_name] = dict() #initialize a dictionary for the current fragment
+                metabolite_dict[metabolite_name]['fragments'] = dict()
+                metabolite_dict[metabolite_name]['peak_profile'] = np.array([])
+                with open(file_name_read, 'r') as metabolite_read_file:
+                    metabolite_line_n = 0
+                    for metabolite_line in metabolite_read_file:
+                        metabolite_line_n = metabolite_line_n + 1
+                        metabolite_line_split = metabolite_line.split(':')
+                        metabolite_line_title = metabolite_line_split[0].lstrip().rstrip()
+                        metabolite_line_item = metabolite_line_split[1].lstrip().rstrip()
+                        if metabolite_line_n == file_line_n + 1:
+                            retention_index = metabolite_line_item
+                            retention_index = np.fromstring(retention_index,dtype=float,sep=' ')
+                            metabolite_dict[metabolite_name]['ri'] = retention_index
+                        if metabolite_line_n == file_line_n + 2:
+                            peak_profile = metabolite_line_item
+                            peak_profile = np.fromstring(peak_profile,dtype=float,sep=' ')
+                            metabolite_dict[metabolite_name]['peak_profile'] = peak_profile
+                        if metabolite_line_title == 'fragment':
+                            with open(file_name_read, 'r') as fragment_read_file:
+                                fragment_line_n = 0
+                                for fragment_line in fragment_read_file:
+                                    fragment_line_n = fragment_line_n + 1
+                                    fragment_line_split = fragment_line.split(':')
+                                    fragment_line_item = fragment_line_split[1].lstrip().rstrip()
+                                    if fragment_line_n == metabolite_line_n:
+                                        fragment_name = fragment_line_item
+                                        metabolite_dict[metabolite_name]['fragments'][fragment_name] = dict()
+                                    if fragment_line_n == metabolite_line_n + 1:
+                                        metabolite_dict[metabolite_name]['fragments'][fragment_name]['formula'] = fragment_line_item
+                                    if fragment_line_n == metabolite_line_n + 2:
+                                        mzs_to_integrate = np.fromstring(fragment_line_item,dtype=float,sep=' ')
+                                        metabolite_dict[metabolite_name]['fragments'][fragment_name]['mzs_to_integrate'] = mzs_to_integrate
 
-
+    metabolite_list = list(metabolite_dict.keys())
+    
     #calculate the natural mass isotopomer distrubutions for each fragment
-    fragment_list = list(dict.keys(fragment_dict))
-    for z in fragment_list:
-        fragment_dict[z]['natural_mid'] = calc_natural_mid.calc_natural_mid(fragment_dict[z]['formula'])
+    #fragment_list = list(metabolite_dict[metabolite_name]['fragments'].keys())
+    #for z in fragment_list:
+    #    metabolite_dict[metabolite_name][z]['natural_mid'] = calc_natural_mid.calc_natural_mid(metabolite_dict[metabolite_name]['fragments'][z]['formula'])
 
-    return(fragment_dict,fragment_list)
+    fragment_list = np.array([])
+    return(metabolite_dict,fragment_list)
